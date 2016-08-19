5b3f697176ee20defd7c4d2bcae136aa034da271
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-08-19 11:21:32 -0300

    Exercise 3.12

--- 3.scm
+++ 3.scm
@@ -443,3 +443,39 @@
 ;; os mesmos objetos de função criados quando definimos acc, sendo portanto
 ;; compartilhados. Isso não afetaria o estado local de acc nem de acc2, já
 ;; que o que houver de diferente entre eles estará definido em frames separados.
+
+;;;;SECTION 3.3
+
+;;;SECTION 3.3.1
+
+(define (cons x y)
+  (let ((new (get-new-pair)))
+    (set-car! new x)
+    (set-cdr! new y)
+    new))
+
+
+;; EXERCISE 3.12
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+;: (define x (list 'a 'b))
+;: (define y (list 'c 'd))
+;: (define z (append  x y))
+;: z
+;: (cdr x)
+;:
+;: (define w (append! x y))
+;: w
+;: (cdr x)
